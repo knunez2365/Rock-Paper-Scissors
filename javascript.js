ae71b580424d955810c8ad921dab184ad2d3b4d8
Knunez2365 <Knunez2365@gmail.com>
2022-08-23 13:21:34 -0400

    deleted comments and fixedscore system

--- javascript.js
+++ javascript.js
@@ -1,53 +1,3 @@
-
-// const DolphinData = [44, 23, 71]
-
-// //const sum = DolphinData.reduce((partialSum, a) => partialSum + a, 0);
-// //console.log(sum); // 6
-
-// const calcAverage = (a, b, c) => (a + b + c) / 3;
-
-
-// const dolphinAvg = calcAverage(85, 54 , 41);
-
-// const  koalaAvg = calcAverage(23, 34, 27);
-
-//function checkWinner(a, b){
-//     if(a >= 2 * b){
-//         winner = " Dolphin is the winner with a score of";
-//         score = a;
-//     } else if (b >= 2 * a){
-//         winner = " Koala is the winner with a score of ";
-//         score = b;
-//     }else{
-//         winner = "No one wins!";
-//         score = " No score, did not reach the points"
-//     }
-//     return (winner, score);
-// } 
-
-// checkWinner(dolphinAvg, koalaAvg);
-// console.log(`${winner}  ${score}`)
-
-
-
-//let playerGuess = prompt('What is your weapon of choice: Rock, Paper, Scissors? ')
-
-
-// function getRandomInt(max) {
-//     return Math.floor(Math.random() * max);
-// }
-
-// function convert(guess){
-//     if(guess === 'rock'){
-//         score = 1;
-//     } else if (guess === 'paper'){
-//         score = 2;
-//     } else if (guess === 'scissors'){
-//         score = 3;
-//     }
-//     return (score);
-// }
-
 const weapons = ["p", "r", "s"];
 
 let computerScore = 0;
@@ -57,79 +7,51 @@ let playerScore = 0;
 function checkWinner(playerGuess = null, computerSelection = null){
     if(computerSelection === playerGuess){
         result = `You Tie! you played ${playerGuess}! The computer played ${computerSelection}`;
+        win = "its a tie"
+
     }else if((computerSelection === "p") && (playerGuess === "r")){
         result = `human looses you played rock! The computer played paper`;
-        computerScore++;
+        win = "computerWin";
     }else if((computerSelection === "r") && (playerGuess === "p")){
         result = `Human wins you played paper! The computer played rock`;
-        playerScore++;
+        win = "playerWin";
     }else if((computerSelection === 's') && (playerGuess === 'p')){
         result = `computer wins you played paper! The computer played scissors`;
-        computerScore++;
+        win = "computerWin";
     }else if((computerSelection === 'p') && (playerGuess === 's')){
         result = `Human wins you played scissors! The computer played paper`;
-        playerGuess++;
+        win = "playerWin";
     }else if((playerGuess === 'r') && (computerSelection === 's')){
         result = `Human wins you played rock! The computer played scissors`;
-        playerScore++;
+        win = "playerWin";
     }else if((playerGuess === 's') && (computerSelection === 'r')){
         result = `Human Looses you played scissors! The computer played rock`;
-        computerScore++;
+        win = "computerWin";
     }else{
         result = `you need a weapon of choice either: paper, rock, scissors`;
+        win = "fix"
     }
-    return(result);
-}
-
-// function gameWinner(gameScore){
-//     if (playerScore <= 5){
-//         announcement = 'Humans won';
-//     }else if (computerScore => 5){
-//         announcement = 'You lost';
-//     }else{
-//         announcement = 'error'
-//     }
-//     return(announcement);
-// }
-
-
-for (let i = 1; playerScore <= 5; i++) {
-    let playerGuess = prompt(('what is your weapon of choice: Rock, Paper, Scissors?')).slice(0,1).toLowerCase();
-    let computerSelection = weapons[Math.floor(Math.random()*weapons.length)];
-    console.log(checkWinner(playerGuess, computerSelection));
+    return(win);
 };
 
 
+function game(roundResult) {
+    if (roundResult === 'playerWin') {
+        playerScore++;
+    } else if (roundResult === 'computerWin') {
+        computerScore++;
+    } else{
+        "its a tie!"
+    }
+};
+
+for (let i = 1; playerScore <= 4; i++) {
+    let playerGuess = prompt(('what is your weapon of choice: Rock, Paper, Scissors?')).slice(0,1).toLowerCase();
+    let computerSelection = weapons[Math.floor(Math.random()*weapons.length)];
+    console.log(checkWinner(playerGuess, computerSelection));
+    console.log(game(win));
+    console.log(`current score ${playerScore} and ${computerScore}`)
+};
 
 
-
-// function checkWinner(Guess){
-//     if(computerSelection === playerGuess){
-//         win = "you tie";
-//     } else if(computerSelection < playerGuess){
-//         win = " you lose";
-//     } else{
-//         win = " you tie";
-//     }
-//     return (win);
-// }
-// console.log(checkWinner(playerGuess));
-// console.log(computerGuess);
-
-// for (let i = 0; i < 5; i++) {
-
-
-// };
-
-
-
-// console.log(playerGuess(prompt('what is your weapon of choice: Rock, Paper, Scissors?')));
-// console.log(checkWinner(playerGuess));
-
-
-
-
-//If a player chooses Rock(0) Paper(1) Scissors(2) we will assign them numbers
-
-//afte we assign them numbers we use math random to generate computer values and compare.
 
